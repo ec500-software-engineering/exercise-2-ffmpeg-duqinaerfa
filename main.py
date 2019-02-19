4273b30262745df8c7e7c64a7964dc7b7d10baf7
Qinmei Du <duqinmei@bu.edu>
2019-02-19 04:12:21 -0500

    update

---
+++ main.py
@@ -0,0 +1,39 @@
+import os
+import subprocess
+import queue
+import threading
+
+input_path = '/Users/duqinmei/Github/exercise-2-ffmpeg-duqinaerfa/input_video'
+output_path = '/Users/duqinmei/Github/exercise-2-ffmpeg-duqinaerfa/output_video'
+
+
+def convert(file):
+    video_720 = "ffmpeg -i " + input_path + "/" + file + " -s hd720 -b:v 1M -r 30 " + output_path + "/" + file[:-4] + "720.mp4"
+    video_480 = "ffmpeg -i " + input_path + "/" + file + " -s hd720 -b:v 1M -r 30 " + output_path + "/" + file[:-4] + "480.mp4"
+    video1 = subprocess.call(video_720, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT, shell=True)
+    print('Convert ' + file + ' to 720P' + ' complete!')
+    video2 = subprocess.call(video_480, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT, shell=True)
+    print('Convert ' + file + ' to 480P' + ' complete!')
+
+
+def main():
+    q = queue.Queue()
+    thread_list = []
+    i = 0
+    
+    try:
+        for file in os.listdir(input_path):
+            if file.endswith('.mp4'):
+                i = i + 1
+                q.put(file)
+                thread_list.append(threading.Thread(target=convert, args=(file, )))
+    except Exception as e:
+        print(e)
+
+    print(str(i) + ' to convert')
+
+    for thread in thread_list:
+        thread.start()
+
+if __name__ == '__main__':
+    main()
