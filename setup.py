b91d28a9f24a17612bfa12eb654e41dc465d86e7
Qinmei Du <duqinmei@bu.edu>
2019-02-27 20:15:06 -0500

    update

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='exercise-2-ffmpeg-duqinaerfa',
+    version='',
+    description='ffmpeg',
+    author='Qinmei Du',
+    author_email='duqinmei@bu.edu',
+    url='',
+    packages=[''],
+
+)
